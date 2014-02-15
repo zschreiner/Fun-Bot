49663a4b5fcbd12168b934194daa5cd648ffc9d4
Tawi T Jordan <aiseiab05@yahoo.com>
2014-02-14 22:37:43 -0800

    Delete Roll.js

--- Case.js/Roll.js
+++
@@ -1,9 +0,0 @@
-Funbot.misc.roll = [
-"You rolled A 1. Bummer :(",
-"You rolled A 2. Bummer :(",
-"You rolled A 3. Bummer :("];
-
-Funbot.misc.roll2 = [
-"4. Awesome!",
-"5. Awesome!",
-"6. Awesome!"];
