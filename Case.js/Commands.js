e0d7275fa24412a977915c03ca70ab9e3b76cf1f
Tawi T Jordan <aiseiab05@yahoo.com>
2014-02-14 22:39:04 -0800

    Delete Commands.js

--- Case.js/Commands.js
+++
@@ -1,3 +0,0 @@
-var commands;
-
-commands = ["reward | flipcoin | catfact | dogfact | hug | 8ball | fortune | help | whywoot | whywoot | props | votes | woot | meh | skip | say | version | userstats | mystats | source | status"]
