df38516b4ed7a16df84ea1516753d97836ec0b82
Tawi T Jordan <aiseiab05@yahoo.com>
2014-03-05 18:26:53 -0800

    Update Fun.js

--- Bot/Fun.js
+++ Bot/Fun.js
@@ -48,7 +48,7 @@ toSave = {};
 toSave.settings = Funbot.settings;
 toSave.moderators = Funbot.moderators;
  
-Funbot.misc.version = "1.0.35";
+Funbot.misc.version = "1.0.37";
 Funbot.misc.origin = "This bot was created by DJ - ɴᴇᴏɴ - TFL, and it is copyrighted!";
 Funbot.misc.ready = true;
 var songBoundary = 60 * 10;
