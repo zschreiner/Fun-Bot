57b5b9331a0980db78a9f9859049029c65437237
Tawi T Jordan <aiseiab05@yahoo.com>
2014-03-08 15:18:48 -0800

    Update Fun.js

--- Bot/Fun.js
+++ Bot/Fun.js
@@ -96,7 +96,7 @@ var blockedArtists = [
 
 // Filter Keywords
 Funbot.filters.beggerWords = ["fanme","fan me","fan4fan","fan 4 fan","fan pls","fans please","need fan","more fan","fan back","give me fans","gimme fans","need fan"];
-Funbot.filters.commandWords = [".reward",".add",".addsong",".flipcoin",".catfact",".dogfact",".hug ",".8ball ",".fortune",".songlink",".download",".help",".whywoot",".whymeh",".props",".votes",".woot",".meh",".say",".version",".userstats @",".mystats",".source",".roomstats",".roomstats2",".register",".join",".leave",".roll"];
+Funbot.filters.commandWords = [".reward",".add",".addsong",".flipcoin",".catfact",".dogfact",".hug",".8ball",".fortune",".songlink",".download",".help",".whywoot",".whymeh",".props",".votes",".woot",".meh",".version",".userstats @",".mystats",".source",".roomstats",".roomstats2",".register",".join",".leave",".roll",".say"];
 
 
 // Fun misc
