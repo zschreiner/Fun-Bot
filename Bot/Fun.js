4dddc333643ba5867fd8cdf68bfc23e281cca850
Tawi T Jordan <aiseiab05@yahoo.com>
2014-03-14 13:03:37 -0700

    Update Fun.js

--- Bot/Fun.js
+++ Bot/Fun.js
@@ -307,7 +307,7 @@ $('#woot').click();
  
 function UserJoin(user)
 {
-var JoinMsg = ["@user has joined!","welcome @user!","Hey there @user!","Glad you came by @user"];
+var JoinMsg = ["@user has joined!","welcome, @user!","Hey there, @user!","Glad you came by, @user"];
 r = Math.floor(Math.random() * JoinMsg.length);
 API.sendChat(JoinMsg[r].replace("user", user.username));
 };
